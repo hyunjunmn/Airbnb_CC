4dea1f0ce35df423468ceb0f64ae0733af11ea96
hyunjunmn <gus6751@gmail.com>
2020-02-11 21:57:07 +0900

    Day11

--- Airbnb_clone_coding/reservations/admin.py
+++ Airbnb_clone_coding/reservations/admin.py
@@ -5,5 +5,17 @@ from . import models
 class ReservationAdmin(admin.ModelAdmin):
     
     #Reservation admin Definition
-    pass
+    list_display = (
+        "room",
+        "status",
+        "check_in",
+        "check_out",
+        "guest",
+        "in_progress",
+        "is_finished"
+    )
+    
+    list_filter = (
+        "status",
+        )
 # Register your models here.
