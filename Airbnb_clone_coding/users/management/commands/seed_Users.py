49d2d23ae01515e5c2e7d9eb6a1a95d2c032c45f
hyunjunmn <gus6751@gmail.com>
2020-03-18 15:25:30 +0900

    seed_rooms

--- Airbnb_clone_coding/users/management/commands/seed_Users.py
+++ Airbnb_clone_coding/users/management/commands/seed_Users.py
@@ -1,5 +1,5 @@
 from django.core.management.base import BaseCommand
-from django.core.management.base import BaseCommand
+from django_seed import Seed
 from users.models import User
 
 class Command(BaseCommand):
@@ -8,13 +8,14 @@ class Command(BaseCommand):
     def add_arguments(self, parser):
         parser.add_argument(
                 "--number",
-                default=1,
+                default=2,
+                type=int,
                 help="How many user do you want create"
         )
               
     def handle(self, *args, **options):
         number = options.get("number")
         seeder = Seed.seeder()
-        seeder.add_entitiy(User,number,{"is_staff":False,"is_superuser":False})
+        seeder.add_entity(User, number, {'is_staff':False,"is_superuser":False})
         seeder.execute()
-        self.stdout.write(self.style.SUCCESS("Amenities created"))
+        self.stdout.write(self.style.SUCCESS(f"{number} users created"))
