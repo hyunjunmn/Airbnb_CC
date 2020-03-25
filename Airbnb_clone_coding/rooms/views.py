dce8d91863f0784e2a52eb3223af9bd375c1e7f2
hyunjunmn <gus6751@gmail.com>
2020-03-25 15:06:16 +0900

    Make_templates

--- Airbnb_clone_coding/rooms/views.py
+++ Airbnb_clone_coding/rooms/views.py
@@ -1,6 +1,9 @@
+from datetime import datetime
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
 def all_rooms(request):
-    return render(request,"all_rooms")
+    now = datetime.now()
+    hungry = True
+    return render(request,"all_rooms.html",context={"now":now,"hungry":hungry})
