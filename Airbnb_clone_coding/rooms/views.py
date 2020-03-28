2e8d7cbb330fc06f49462b148c5231503c8f92c3
hyunjunmn <gus6751@gmail.com>
2020-03-28 18:52:59 +0900

    offset_page

--- Airbnb_clone_coding/rooms/views.py
+++ Airbnb_clone_coding/rooms/views.py
@@ -1,8 +1,11 @@
-from datetime import datetime
 from django.shortcuts import render
 from . import models
 # Create your views here.
 
 def all_rooms(request):
-    all_rooms = models.Room.objects.all()
+    page = int(request.GET.get("page",1))
+    page_size = 10
+    limit = page_size *page
+    offset = limit-page_size
+    all_rooms = models.Room.objects.all()[offset:limit]
     return render(request,"rooms/home.html",context={"rooms": all_rooms})
