78dc912c5854112d47e519de74fa1f6f7c28f108
hyunjunmn <gus6751@gmail.com>
2020-03-31 19:05:00 +0900

    How_to_use_paginator_in_page

--- Airbnb_clone_coding/rooms/views.py
+++ Airbnb_clone_coding/rooms/views.py
@@ -5,9 +5,9 @@ from . import models
 # Create your views here.
 
 def all_rooms(request):
-    page = int(request.GET.get("page"))
+    page = int(request.GET.get("page",1))
     room_list = models.Room.objects.all()  #query sets
-    paginator = Paginator(room_list, 10)
-    rooms = paginator.get_page(page)
-    print(vars(rooms.paginator))
-    return render(request,"rooms/home.html",{"rooms":rooms})
+    paginator = Paginator(room_list, 10,orphans=5) #orpahns남은 자료들을 그 전페이지로 이어붙임
+    rooms = paginator.page(int(page))
+  #  print(vars(rooms.paginator))
+    return render(request,"rooms/home.html",{"page":rooms})
