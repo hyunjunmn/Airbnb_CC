3b2390dbbdd2fb29a3d67cf8a1a9be4d9f4711f3
hyunjunmn <gus6751@gmail.com>
2020-04-01 21:04:13 +0900

    Class_based_views

--- Airbnb_clone_coding/rooms/views.py
+++ Airbnb_clone_coding/rooms/views.py
@@ -1,13 +1,25 @@
-from math import ceil
-from django.shortcuts import render
-from django.core.paginator import Paginator
+from django.views.generic import ListView
 from . import models
 # Create your views here.
 
-def all_rooms(request):
+class HomeView(ListView):
+      """Home View Definition"""
+      
+      model = models.Room
+      paginate_by =10
+      paginate_orphans = 5
+      ordering = "created"
+     # page_kwarg = "page"
+
+'''def all_rooms(request):
     page = int(request.GET.get("page",1))
     room_list = models.Room.objects.all()  #query sets
     paginator = Paginator(room_list, 10,orphans=5) #orpahns남은 자료들을 그 전페이지로 이어붙임
-    rooms = paginator.page(int(page))
+    try:
+        rooms = paginator.page(int(page))
+        return render(request,"rooms/home.html",{"page":rooms})
+    except EmptyPage:
+      return redirect("/")
+      #rooms = paginator.page(1)
   #  print(vars(rooms.paginator))
-    return render(request,"rooms/home.html",{"page":rooms})
+    '''
