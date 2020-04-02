0b5528d86988492fd236539c8ea2f405dcbf984c
hyunjunmn <gus6751@gmail.com>
2020-04-02 19:28:48 +0900

    URL_tags

--- Airbnb_clone_coding/rooms/views.py
+++ Airbnb_clone_coding/rooms/views.py
@@ -1,4 +1,5 @@
 from django.views.generic import ListView
+from django.shortcuts import render
 from . import models
 # Create your views here.
 
@@ -9,7 +10,21 @@ class HomeView(ListView):
       paginate_by =10
       paginate_orphans = 5
       ordering = "created"
+      context_object_name = "rooms"
      # page_kwarg = "page"
+     
+def room_detail(request,pk):
+      print(pk)
+      return render(request,"rooms/detail.html")
+      
+
+"""def get_context_data(self, **kwargs):
+          context =super().get_context_data(**kwargs)
+          now = timezone.now()
+          context["now"] = now
+          return context """
+      #how to use class based view
+
 
 '''def all_rooms(request):
     page = int(request.GET.get("page",1))
