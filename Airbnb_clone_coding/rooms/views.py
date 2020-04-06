4ff0990a8033eb50a602a12438a3dd9efe5d2c6c
hyunjunmn <gus6751@gmail.com>
2020-04-06 10:14:50 +0900

    search_bar

--- Airbnb_clone_coding/rooms/views.py
+++ Airbnb_clone_coding/rooms/views.py
@@ -1,5 +1,4 @@
-from django.views.generic import ListView
-from django.http import Http404
+from django.views.generic import ListView,DetailView
 from django.shortcuts import render
 from . import models
 # Create your views here.
@@ -14,12 +13,22 @@ class HomeView(ListView):
       context_object_name = "rooms"
      # page_kwarg = "page"
      
-def room_detail(request,pk):
+class RoomDetail(DetailView):
+    model = models.Room
+ 
+
+def search(request):
+    city = request.GET.get("city")
+    city = str.capitalize(city)
+    return render(request,"rooms/search.html",{"city":city})
+    
+    
+"""def room_detail(request,pk):
     try:
         room = models.Room.objects.get(pk=pk)
         return render(request,"rooms/detail.html",{'room':room})
 
     except models.Room.DoesNotExist:
         raise Http404()
-    
+   """ 
     
