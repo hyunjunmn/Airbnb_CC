2cf5f13d0ce151b76779587cb7e55b6e8380e4f7
hyunjunmn <gus6751@gmail.com>
2020-04-10 11:54:35 +0900

    Finish_Form

--- Airbnb_clone_coding/rooms/views.py
+++ Airbnb_clone_coding/rooms/views.py
@@ -29,11 +29,13 @@ def search(request):
     bedrooms = int(request.GET.get("bedrooms",0))
     beds = int(request.GET.get("beds",0))
     baths = int(request.GET.get("baths",0))
-    s_amenities = request.GET.get("amenities")
-    s_facilities = request.GET.get("facilities")
+    instant=request.GET.get("instant",False)
+    superhost=request.GET.get("superhost",False)
+    s_amenities = request.GET.getlist("amenities")
+    s_facilities = request.GET.getlist("facilities")
         
     form = {"city":city,"s_room_type":room_type,"s_country":country,"price":price,"guests":guests,"bedrooms":bedrooms,
-             "beds":beds,"baths":baths}
+             "beds":beds,"baths":baths,"s_amenities":s_amenities,"s_facilities":s_facilities,"instant":instant,"superhost":superhost}
   
     room_types=models.RoomType.objects.all()  
     amenities = models.Amenity.objects.all()
