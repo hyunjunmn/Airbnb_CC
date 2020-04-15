e285482654cdefbdfca8914d645ac2cea96f42ba
hyunjunmn <gus6751@gmail.com>
2020-04-15 13:05:43 +0900

    filter_#2

--- Airbnb_clone_coding/rooms/views.py
+++ Airbnb_clone_coding/rooms/views.py
@@ -29,8 +29,8 @@ def search(request):
     bedrooms = int(request.GET.get("bedrooms",0))
     beds = int(request.GET.get("beds",0))
     baths = int(request.GET.get("baths",0))
-    instant=request.GET.get("instant",False)
-    superhost=request.GET.get("superhost",False)
+    instant=bool(request.GET.get("instant",False))
+    superhost=bool(request.GET.get("superhost",False))
     s_amenities = request.GET.getlist("amenities")
     s_facilities = request.GET.getlist("facilities")
         
@@ -52,8 +52,32 @@ def search(request):
     if room_type != 0:
         filter_args["room_type__pk"] = room_type
         
-    print(filter_args)
+    if price !=0:
+        filter_args["price__lte"] = price
+        
+    if guests !=0:
+        filter_args["guests__gte"] = guests
+        
+    if beds !=0:
+        filter_args["bedrooms__gte"] =beds
+        
+    if baths !=0:
+        filter_args["baths__gte"] = baths
+        
+    if instant is True:
+        filter_args["instant_book"] = True
     
+    if superhost is True:
+        filter_args["host__superhost"] = True
+    
+    if len(s_amenities) > 0:
+        for s_amenity in s_amenities:
+            filter_args["amenities__pk"] = int(s_amenity)
+            
+    if len(s_facilities) > 0:
+        for s_facility in s_facilities:
+            filter_args["facilities__pk"] = int(s_facility)
+            
     rooms = models.Room.objects.filter(**filter_args)
 
     
